511c1e0dadc8b8fe4f857f84e75f1ee54884fe09
Tatziki <contact@tatziki.com>
2024-09-12 23:11:58 +0200

    Vite config avec ajout de data dans public

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => {
+  return {
   plugins: [react()],
-  base: '/LAGUNEN-ETXEA/',  // Définir le chemin de base pour le déploiement sur GitHub Pages
+  base: mode === 'production' ? '/LAGUNEN-ETXEA/' : '/',  // En prod: avec préfixe, en dev: sans  // Définir le chemin de base pour le déploiement sur GitHub Pages
+  }
 });
