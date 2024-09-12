d2291565cef47d6ee04be4f282706bcee8eb38b0
Tatziki <contact@tatziki.com>
2024-09-12 22:31:44 +0200

    Suite build + deploy

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -2,14 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import Maintenance from "./Maintenance/Maintenance";
 import Main from "./Maintenance/Main";
 import { SheetDataProvider } from "./Context/DataContext";
-import './globalfix';
+import './globalFix';
 
 function App() {
   return (
     <SheetDataProvider>
       <Routes>
-        <Route path="/" element={<Maintenance />}/>
-        <Route path="app/" element={<Main />}/>
+        <Route path="/temp" element={<Maintenance />}/>
+        <Route path="" element={<Main />}/>
       </Routes>
     </SheetDataProvider>
   );
